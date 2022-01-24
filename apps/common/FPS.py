7689efe239d612ef25b687701d3deecd57152d9f
EndMah <endrika.mahaputra@gmail.com>
2022-01-24 23:28:26 +0100

    MOT16 .mp4 files, mp4 stream test and .txt results

--- apps/common/FPS.py
+++ apps/common/FPS.py
@@ -27,12 +27,18 @@ class GETFPS:
         global frame_count
         self.frame_count=frame_count
         self.stream_id=stream_id
+    def get_fps(self):
+        end_time=time.time()
+        if(self.is_first):
+            self.start_time=end_time
+            self.is_first=False
+        if(end_time-self.start_time>5):
+            print("**********************FPS*****************************************")
+            print("Fps of stream",self.stream_id,"is ", float(self.frame_count)/5.0)
+            self.frame_count=0
+            self.start_time=end_time
+        else:
+            self.frame_count=self.frame_count+1
     def print_data(self):
         print('frame_count=',self.frame_count)
         print('start_time=',self.start_time)
-    def calc_fps(self):
-        end_time=time.time()
-        elapsed_time = end_time - self.start_time
-        current_fps = 1.0/float(elapsed_time)
-        self.start_time=end_time
-        return current_fps
